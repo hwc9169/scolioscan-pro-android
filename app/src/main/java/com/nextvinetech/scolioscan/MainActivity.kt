62d44de33de12f5dc2f3400c3bb871dbc5d8d96a
hwc9169 <hwc9169@gmail.com>
2025-12-11 12:57:08 +0900

    더 좋은 모델 사용

--- app/src/main/java/com/nextvinetech/scolioscan/MainActivity.kt
+++ app/src/main/java/com/nextvinetech/scolioscan/MainActivity.kt
@@ -258,7 +258,7 @@ class PoseAnalyzer(context: Context, poseGuideline: PoseGuideline): ImageAnalysi
   private val landmarker: PoseLandmarker
 
   init {
-    val modelName = "models/pose_landmarker_lite.task"
+    val modelName = "models/pose_landmarker_full.task"
     val base = BaseOptions.builder()
       .setModelAssetPath(modelName)
       .setDelegate(Delegate.CPU)
@@ -266,8 +266,8 @@ class PoseAnalyzer(context: Context, poseGuideline: PoseGuideline): ImageAnalysi
 
     val options = PoseLandmarker.PoseLandmarkerOptions.builder()
       .setBaseOptions(base)
-      .setMinPoseDetectionConfidence(0.4f)
-      .setMinTrackingConfidence(0.4f)
+      .setMinPoseDetectionConfidence(0.5f)
+      .setMinTrackingConfidence(0.5f)
       .setRunningMode(RunningMode.LIVE_STREAM)
       .setNumPoses(1)
       .setResultListener { result: PoseLandmarkerResult, _: MPImage? ->
